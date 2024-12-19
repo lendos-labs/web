aab35f656b092de77594a5ddf3b826c3aad90451
VasylykSerhiy <vasylyk29@gmail.com>
2024-12-19 18:10:29 +0200

    create theme

--- packages/ui/src/layouts/MuiLayout/index.tsx
+++ packages/ui/src/layouts/MuiLayout/index.tsx
@@ -1,10 +1,55 @@
 'use client';
+import React, { createContext, ReactNode, useEffect, useMemo, useState } from 'react';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import { useMediaQuery } from '@mui/material';
+import { getDesignTokens, getThemedComponents } from '../../theme/theme';
+import { deepmerge } from '@mui/utils';
+import CssBaseline from '@mui/material/CssBaseline';
 
-import { ThemeProvider } from '@mui/material';
-import React, { ReactNode } from 'react';
-import { theme } from '../../theme/theme';
+export const ColorModeContext = createContext({
+  toggleColorMode: () => {},
+});
+
+type Mode = 'light' | 'dark';
 
 export const MuiLayout = ({ children }: Readonly<{ children: ReactNode }>) => {
-  /* eslint-disable @typescript-eslint/no-unsafe-assignment -- TODO fix it */
-  return <ThemeProvider theme={theme}>{children}</ThemeProvider>;
+  const prefersDarkMode = useMediaQuery('(prefers-color-scheme: dark)');
+  const [mode, setMode] = useState<Mode>(prefersDarkMode ? 'dark' : 'light');
+
+  const colorMode = useMemo(
+    () => ({
+      toggleColorMode: () => {
+        setMode(prevMode => {
+          const newMode = prevMode === 'light' ? 'dark' : 'light';
+          document.cookie = `colorMode=${newMode};path=/`;
+          // Cookies.set('colorMode', newMode);
+          return newMode;
+        });
+      },
+    }),
+    [],
+  );
+
+  const theme = useMemo(() => {
+    const themeCreate = createTheme(getDesignTokens(mode));
+    return deepmerge(themeCreate, getThemedComponents(themeCreate));
+  }, [mode]);
+
+  useEffect(() => {
+    const cookieMode = document.cookie.split('; ').find(row => row.startsWith('colorMode=')) as
+      | Mode
+      | undefined;
+    const initialMode = cookieMode ?? (prefersDarkMode ? 'dark' : 'light');
+
+    setMode(initialMode);
+  }, [prefersDarkMode]);
+
+  return (
+    <ColorModeContext.Provider value={colorMode}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        {children}
+      </ThemeProvider>
+    </ColorModeContext.Provider>
+  );
 };
